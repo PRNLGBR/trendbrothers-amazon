9aaa5451f800db357fc3c75cf21f1974787bef9c
PRNLGBR <99623826+PRNLGBR@users.noreply.github.com>
2026-02-02 15:33:31 -0400

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-8">
+      <p>© 2026 TrendBrothers. Todos os direitos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
