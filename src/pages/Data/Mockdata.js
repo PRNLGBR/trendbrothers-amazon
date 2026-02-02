a098692ec5e507ffcdd1a77f2c4e434e64565e9c
PRNLGBR <99623826+PRNLGBR@users.noreply.github.com>
2026-02-02 13:51:50 -0400

    Create Mockdata.js

---
+++ src/pages/Data/Mockdata.js
@@ -0,0 +1,16 @@
+export const products = [
+  {
+    id: 1,
+    title: "Smartwatch Bluetooth com Monitor Cardíaco",
+    price: 199.90,
+    image: "https://m.media-amazon.com/images/I/61lKQWyMdDL._AC_SX679_.jpg",
+    category: "Eletrônicos",
+    rating: 4.6,
+    reviews: 321,
+    amazonLink: "https://www.amazon.com.br/"
+  },
+  {
+    id: 2,
+    title: "Fone de Ouvido Bluetooth 5.3",
+    price: 149.90,
+    image: "https://m.media-amazon.com/images/I/61CGHv6
